ba9949db3caa7176f72c751394cbdf55d7e9cac2
$seafarer111 <92005974+seafarer111@users.noreply.github.com>
2024-05-03 12:57:55 -0700

    feat: update

--- server/config.js
+++ server/config.js
@@ -31,6 +31,6 @@ module.exports = {
       withdrawFee: "0.00001",
     },
   },
-  SUBSCRIBE_URL: "http://185.190.140.99:5000/api/payments/webhook-handler",
+  SUBSCRIBE_URL: "http://localhost:5000/api/payments/webhook-handler",
   DEV_MDOE: true,
 };
